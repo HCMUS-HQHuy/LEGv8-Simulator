af9263377ee01e36ed0078ee28a268b26c64da8a
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-04-14 10:47:20 +0700

    initialize project

---
+++ src/main/java/com/example/legv8sim/LeGv8simApplication.java
@@ -0,0 +1,13 @@
+package com.example.legv8sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeGv8simApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeGv8simApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/legv8sim/LeGv8simApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.legv8sim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeGv8simApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
