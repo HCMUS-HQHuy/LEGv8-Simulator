4d6c35587433a435fb55fa9f3da050552308af70
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-04-22 17:43:06 +0700

    nothong

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,4 @@
-import * as handleReceivInstruction from "./InstructionHandle/FormSubmit.js"
+import * as handleReceivInstruction from "./InstructionHandle/formSubmit.js"
 import * as ZoomDragAndDrop from "./HandleFrame/ZoomDragAndDrop.js"
 import * as SwitchThemes from "./HandleOutLook/Themes.js"
 import * as instructionLine from "./HandleOutLook/InstructionCode.js"
