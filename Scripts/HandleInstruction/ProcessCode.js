651ef08adb663816398fef104126c98df9067160
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-04-23 23:09:43 +0700

    add event to replay botton

--- Scripts/HandleInstruction/ProcessCode.js
+++ Scripts/HandleInstruction/ProcessCode.js
@@ -20,5 +20,23 @@ export function trigger() {
 		fetching.trigger(parsedInstruction);
 		
 	});
+
+	const restartButton = document.getElementById('start-animation');
+	restartButton.addEventListener('click', function(event) {
+		event.preventDefault();
+
+		const results = formatCode.getResult();
+		if (results == null) {
+			console.error("formatcode: Have some problem!");
+			return;
+		}
+		parsedOutputTable.update(results);
+		
+		// // --- LẤY GIÁ TRỊ PC HIỆN TẠI TRƯỚC KHI TĂNG ---
+		// const pcValueForFetch = currentPC;
+		// console.log(`PC incremented to: 0x${currentPC.toString(16).toUpperCase().padStart(8,'0')}`);
+		const parsedInstruction = results[0].parsed;
+		fetching.trigger(parsedInstruction);
+	});
 }
 
