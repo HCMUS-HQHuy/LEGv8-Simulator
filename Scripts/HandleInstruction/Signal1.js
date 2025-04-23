8b08d5b26b88f973044dc1f62743fb65994924d0
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-04-23 17:44:43 +0700

    Modify filename

--- Scripts/HandleInstruction/Signal1.js
+++ Scripts/HandleInstruction/Signal1.js
@@ -1,5 +1,5 @@
-import controlSignalTable from "./defineSignal.js";
-import signalDestinations from "./signalDestinations.js";
+import controlSignalTable from "./DefineSignal1.js";
+import signalDestinations from "./SignalDestinations1.js";
 
 
 const svgNS = "http://www.w3.org/2000/svg";
