b832acd2e036b5f347bb8b34fd4bf9a12c0f84d2
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-04-24 00:56:40 +0700

    processCode

--- Scripts/HandleInstruction/ProcessCode.js
+++ Scripts/HandleInstruction/ProcessCode.js
@@ -4,41 +4,27 @@ import * as fetch from "./Execute/fetch.js"
 import * as decode from "./Execute/decode.js"
 import * as execute from "./Execute/execute.js"
 
+function processCode() {
+	const results = formatCode.getResult();
+	if (results == null) {
+		console.error("formatcode: Have some problem!");
+		return;
+	}
+	parsedOutputTable.update(results);
+	const parsedInstruction = results[0].parsed;
+	fetch.trigger(decode.trigger(parsedInstruction, execute.trigger(parsedInstruction)));
+}
+
 export function trigger() {
 	codeForm.addEventListener('submit', function(event) {
         event.preventDefault();
-
-		const results = formatCode.getResult();
-		if (results == null) {
-			console.error("formatcode: Have some problem!");
-			return;
-		}
-		parsedOutputTable.update(results);
-		
-		// // --- LẤY GIÁ TRỊ PC HIỆN TẠI TRƯỚC KHI TĂNG ---
-		// const pcValueForFetch = currentPC;
-		// console.log(`PC incremented to: 0x${currentPC.toString(16).toUpperCase().padStart(8,'0')}`);
-		const parsedInstruction = results[0].parsed;
-		fetch.trigger(decode.trigger(parsedInstruction, execute.trigger(parsedInstruction)));
-		
+		processCode();		
 	});
 
 	const restartButton = document.getElementById('start-animation');
 	restartButton.addEventListener('click', function(event) {
 		event.preventDefault();
-
-		const results = formatCode.getResult();
-		if (results == null) {
-			console.error("formatcode: Have some problem!");
-			return;
-		}
-		parsedOutputTable.update(results);
-		
-		// // --- LẤY GIÁ TRỊ PC HIỆN TẠI TRƯỚC KHI TĂNG ---
-		// const pcValueForFetch = currentPC;
-		// console.log(`PC incremented to: 0x${currentPC.toString(16).toUpperCase().padStart(8,'0')}`);
-		const parsedInstruction = results[0].parsed;
-		fetch.trigger(parsedInstruction);
+		processCode();	
 	});
 }
 
